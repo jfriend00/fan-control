69caaaa788e35f2cf3de8c49ddae40526d55f9a9
jfriend00 <github@the-friend-family.com>
2014-09-17 00:46:38 -0700

    Fix copying of view files

--- pi.bat
+++ pi.bat
@@ -1,1 +1,2 @@
-pscp -p -pw raspberry d:\code\GitHub\fan-control\src\*.js d:\code\GitHub\fan-control\*.js d:\code\GitHub\fan-control\*.sh d:\code\GitHub\pi-gpio\*.js d:\code\GitHub\fan-control\views\*.html pi@192.168.1.155:/home/pi
+pscp -p -pw raspberry d:\code\GitHub\fan-control\src\*.js d:\code\GitHub\fan-control\*.js d:\code\GitHub\fan-control\*.sh d:\code\GitHub\pi-gpio\*.js pi@192.168.1.155:/home/pi
+pscp -p -pw raspberry d:\code\GitHub\fan-control\views\*.html pi@192.168.1.155:/home/pi/views

d51288db7e041ebed77a5d3b02af01919629034b
jfriend00 <github@the-friend-family.com>
2014-09-18 23:30:59 -0700

    Add partials

--- pi.bat
+++ pi.bat
@@ -1,2 +1,3 @@
 pscp -p -pw raspberry d:\code\GitHub\fan-control\src\*.js d:\code\GitHub\fan-control\*.js d:\code\GitHub\fan-control\*.sh d:\code\GitHub\pi-gpio\*.js pi@192.168.1.155:/home/pi
 pscp -p -pw raspberry d:\code\GitHub\fan-control\views\*.html pi@192.168.1.155:/home/pi/views
+pscp -p -pw raspberry d:\code\GitHub\fan-control\views\partials\*.html pi@192.168.1.155:/home/pi/views/partials

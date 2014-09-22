dd705ecfc355dc20c50c5f2f185e197da086d633
jfriend00 <github@the-friend-family.com>
2014-09-21 19:18:14 -0700

    Add /lib directory

--- pi.bat
+++ pi.bat
@@ -1,2 +1,2 @@
 pscp -p -pw raspberry d:\code\GitHub\fan-control\src\*.js d:\code\GitHub\fan-control\*.js d:\code\GitHub\fan-control\*.sh d:\code\GitHub\pi-gpio\*.js pi@192.168.1.155:/home/pi
-pscp -r -p -pw raspberry d:\code\GitHub\fan-control\views pi@192.168.1.155:/home/pi
+pscp -r -p -pw raspberry d:\code\GitHub\fan-control\views d:\code\GitHub\fan-control\lib pi@192.168.1.155:/home/pi

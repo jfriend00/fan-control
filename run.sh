ab8a61a9b6e06a3167915ec06c44e5062b296c76
jfriend00 <github@the-friend-family.com>
2014-09-25 00:10:06 -0700

    Add ability to bypass auto-startup of fan-control.js

--- run.sh
+++ run.sh
@@ -3,9 +3,17 @@
 # Make sure we only auto-start the fan-control just once
 PIDS=`ps aux | grep fan-control.js | grep -v grep`
 if [ -z "$PIDS" ]; then
-    echo "starting initial fan-control.js ..."
-    cd /home/pi
-    forever -a --minUptime=3000 --spinSleepTime=1000 --killSignal=SIGTERM -l  /home/pi/logs/fan-control.log -e /home/pi/logs/fan-control.err --sourceDir=/home/pi start fan-control.js
+    # prompt to bypass fan-control startup (with auto timeout)
+    read -s -n 1 -p "Press n to cancel start of fan-control.js" -t 2 ANSWER
+    echo
+    if [[ $ANSWER != [nN] ]]; then
+        echo "starting initial fan-control.js ..."
+        cd /home/pi
+        forever -a --minUptime=3000 --spinSleepTime=1000 --killSignal=SIGTERM -l  /home/pi/logs/fan-control.log -e /home/pi/logs/fan-c$
+    else
+        echo "skipping fan-control.js"
+    fi
 else
     echo "fan-control.js already running"
+
 fi

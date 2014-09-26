4ec9201a65e803709e493bc5b0b68dafba0754e7
jfriend00 <github@the-friend-family.com>
2014-09-25 23:38:55 -0700

    Add prompt to bypass auto-start

--- run.sh
+++ run.sh
@@ -3,16 +3,18 @@
 # Make sure we only auto-start the fan-control just once
 PIDS=`ps aux | grep fan-control.js | grep -v grep`
 if [ -z "$PIDS" ]; then
-    # prompt to bypass fan-control startup (with auto timeout)
-    read -s -n 1 -p "Press n to cancel start of fan-control.js" -t 2 ANSWER
-    echo
-    if [[ $ANSWER != [nN] ]]; then
-        echo "starting initial fan-control.js ..."
-        cd /home/pi
-        forever -a --minUptime=3000 --spinSleepTime=1000 --killSignal=SIGTERM -l  /home/pi/logs/fan-control.log -e /home/pi/logs/fan-control.err --sourceDir=/home/pi start fan-control.js
-    else
-        echo "skipping fan-control.js"
-    fi
+	if [[ "$1" == "prompt" ]]; then
+		# prompt to bypass fan-control startup (with auto timeout)
+		read -s -n 1 -p "Press n to cancel start of fan-control.js" -t 3 ANSWER
+	fi
+	echo
+	if [[ $ANSWER != [nN] ]]; then
+		echo "starting initial fan-control.js ..."
+		cd /home/pi
+		forever -a --minUptime=3000 --spinSleepTime=1000 --killSignal=SIGTERM -l  /home/pi/logs/fan-control.log -e /home/pi/logs/fan-control.err --sourceDir=/home/pi start fan-control.js
+	else
+		echo "skipping fan-control.js"
+	fi
 else
     echo "fan-control.js already running"
 

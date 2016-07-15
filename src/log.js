800aae810bc81f0ddeedfc2463e4afec794790f3
jfriend00 <github@the-friend-family.com>
2016-07-15 16:14:31 -0700

    Add log frequency option

--- src/log.js
+++ src/log.js
@@ -23,10 +23,46 @@ function log(level, ...args) {
 }
 */
 
-function log(level) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    var output = [level];
+
+var tags = {};
+var dayMs = 1000 * 60 * 60 * 24;
+
+// use an interval timer to clear out tags older than a day 
+// note - if the server is configured to restart every day at 4am, then this code will never run
+// because the server never actually has more than 24hrs of uptime
+setInterval(function() {
+    var now = Date.now();
+    Object.keys(tags).forEach(function(tag) {
+        if (now - tags[tag] > dayMs) {
+            delete tags[tag];
+        }
+    });
+}, dayMs + (1000*60*10));
+
+// can be used as:
+//  log(3, "Some msg");
+//  log({tag: "waiting", level: 3, delta: xxx}, "Some msg");
+//  log({level: 3, delta: xxx}, "Some msg");         if tag is not present, then first msg is used as the tag
+function log(options) {
+    var output = [];
     try {
+        var args = Array.prototype.slice.call(arguments, 1);
+        if (typeof options === "object") {
+            output.push(options.level);
+            let tag = options.tag || args[0];
+            let now = Date.now();
+            let priorTime = tags[tag];
+            if (!priorTime || now - priorTime > options.delta) {
+                // update the tag time and let the normal processing occur
+                tags[tag] = now;
+            } else {
+                // not enough time has passed to show this message
+                return;
+            }
+        } else {
+            // must be just plain level value
+            output.push(options);
+        }
         output.push(new Date().toString());
         args.forEach(function(item) {
             var t = typeof item;
@@ -41,6 +77,5 @@ function log(level) {
     }
     console.log(output.join("; "));
 }
-
 
 module.exports = log;
